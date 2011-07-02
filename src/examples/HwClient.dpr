0d33b9138177117d0de2e62439b52bf67526973a
Rafa Leblic <rleblic@gmail.com>
2011-07-02 22:02:20 +0200

    Replace move for CopyMemory, Initialize PZMQMsg pointers

--- src/examples/HwClient.dpr
+++ src/examples/HwClient.dpr
@@ -6,40 +6,46 @@ program HwClient;
 {$APPTYPE CONSOLE}
 
 uses
-  SysUtils, zmq;
+  SysUtils, zmq, Windows;
 
 var
-  context, requester, msg_data: Pointer;
+  context, requester: Pointer;
   request_nbr: Integer;
-  request, reply: TZMQMsg;
-
+  request, reply: PZMQMsg;
 begin
+  GetMem(request, SizeOf(request));
+  GetMem(reply, SizeOf(reply));
+
   try
-    context := zmq_init(1);
+    try
+      context := zmq_init(1);
 
-    writeln('Connecting to hello world server...');
-    requester := zmq_socket(context, ZMQ_REQ);
-    zmq_connect(requester, 'tcp://localhost:5555');
+      writeln('Connecting to hello world server...');
+      requester := zmq_socket(context, ZMQ_REQ);
+      zmq_connect(requester, 'tcp://localhost:5555');
 
-    for request_nbr := 0 to 9 do
-    begin
-      zmq_msg_init_size(@request, 5);
-      msg_data := zmq_msg_data(@request);
-      move('Hello', msg_data, 5);
-      writeln(format('Sending Hello %d...', [request_nbr]));
-      zmq_send(requester, @request, 0);
-      zmq_msg_close(@request);
+      for request_nbr := 0 to 9 do
+      begin
+        zmq_msg_init_size(request, 5);
+        CopyMemory(zmq_msg_data(request), PChar('Hello'), 5);
+        writeln(format('Sending Hello %d...', [request_nbr]));
+        zmq_send(requester, request, 0);
+        zmq_msg_close(request);
 
-      zmq_msg_init(@reply);
-      zmq_recv(requester, @reply, 0);
-      writeln(format('Received World %d...', [request_nbr]));
-      zmq_msg_close(@reply);
+        zmq_msg_init(reply);
+        zmq_recv(requester, reply, 0);
+        writeln(format('Received World %d...', [request_nbr]));
+        zmq_msg_close(reply);
+      end;
+
+      zmq_close(requester);
+      zmq_term(context);
+    except
+      on E:Exception do
+        Writeln(E.Classname, ': ', E.Message);
     end;
-
-    zmq_close(requester);
-    zmq_term(context);
-  except
-    on E:Exception do
-      Writeln(E.Classname, ': ', E.Message);
+  finally
+    FreeMem(reply);
+    FreeMem(request);
   end;
 end.
--- src/examples/HwServer.dpr
+++ src/examples/HwServer.dpr
@@ -6,40 +6,46 @@ program HwServer;
 {$APPTYPE CONSOLE}
 
 uses
-  SysUtils, zmq;
+  SysUtils, zmq, windows;
 
 var
-  context, responder, msg_data: Pointer;
-  request, reply: TZMQMsg;
-  
+  context, responder: Pointer;
+  request, reply: PZMQMsg;
 begin
+  GetMem(request, SizeOf(request));
+  GetMem(reply, SizeOf(reply));
+
   try
-    context := zmq_init(1);
+    try
+      context := zmq_init(1);
 
-    responder := zmq_socket(context, ZMQ_REP);
-    zmq_bind(responder, 'tcp://*:5555');
+      responder := zmq_socket(context, ZMQ_REP);
+      zmq_bind(responder, 'tcp://*:5555');
 
-    while true do
-    begin
-      zmq_msg_init(@request);
-      zmq_recv(responder, @request, 0);
-      writeln('Received Hello...');
-      zmq_msg_close(@request);
+      while true do
+      begin
+        zmq_msg_init(request);
+        zmq_recv(responder, request, 0);
+        writeln('Received Hello...');
+        zmq_msg_close(request);
 
-      sleep(1);
+        sleep(1);
 
-      zmq_msg_init_size(@reply, 5);
-      msg_data := zmq_msg_data(@reply);
-      move('World', msg_data, 5);
-      zmq_send(responder, @reply, 0);
-      zmq_msg_close(@reply);
+        zmq_msg_init_size(reply, 5);
+        CopyMemory(zmq_msg_data(reply), PChar('World'), 5);
+        zmq_send(responder, reply, 0);
+        zmq_msg_close(reply);
+      end;
+
+      zmq_close(responder);
+      zmq_term(context);
+
+    except
+      on E:Exception do
+        Writeln(E.Classname, ': ', E.Message);
     end;
-
-    zmq_close(responder);
-    zmq_term(context);
-
-  except
-    on E:Exception do
-      Writeln(E.Classname, ': ', E.Message);
+  finally
+    FreeMem(reply);
+    FreeMem(request);
   end;
 end.
