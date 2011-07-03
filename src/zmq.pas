b2bfa4c98fd98e85905f2a9a84bf498249e0f0ec
Rafa Leblic <rleblic@gmail.com>
2011-07-03 19:23:27 +0200

    Add platform compiler directives, remove zmq c header prototype comments

--- src/zmq.pas
+++ src/zmq.pas
@@ -22,11 +22,16 @@
 unit zmq;
 
 interface
+{$IFDEF WINDOWS}
 uses
   Winsock;
-  
+{$ENDIF}
+
 const
+
+{$IFDEF WINDOWS}
 LIBZMQ = 'libzmq.dll';
+{$ENDIF}
 
 {-----------------------------------}
 {-----------------------------------}
@@ -34,7 +39,6 @@ LIBZMQ = 'libzmq.dll';
 {-----------------------------------}
 ZMQ_HAUSNUMERO = 156384712;
 
-(* On Windows platform some of the standard POSIX errnos are not defined. *)
 ENOTSUP = (ZMQ_HAUSNUMERO + 1);
 EPROTONOSUPPORT = (ZMQ_HAUSNUMERO + 2);
 ENOBUFS = (ZMQ_HAUSNUMERO + 3);
@@ -129,71 +133,56 @@ ZMQ_FORWARDER = 2;
 ZMQ_QUEUE = 3;
 
 type
+
 TZMQMsg = packed record
   content: Pointer;
   flags: Byte;
   vsm_size: Byte;
   vsm_data: Array [0..ZMQ_MAX_VSM_SIZE-1] of Char;
 end;
+
 PZMQMsg = ^TZMQMsg;
 
 TZMQFreeFn = procedure(data, hint: Pointer);
 
 TZMQPollItem = packed record
   socket: Pointer;
+{$IFDEF WINDOWS}
   fd: TSocket;
+{$ELSE}
+  fd: Integer;
+{$ENDIF}
   events: ShortInt;
   revents: ShortInt;
 end;
+
 PZMQPollItem = ^TZMQPollItem;
 
 {-----------------------------------}
 {-----------------------------------}
 { Run-time API version detection.   }
 {-----------------------------------}
-// ZMQ_EXPORT void zmq_version (int *major, int *minor, int *patch);
 procedure zmq_version(major, minot, patch: Integer); cdecl; external LIBZMQ;
 
 {-----------------------------------}
 {-----------------------------------}
 { Error handling                    }
 {-----------------------------------}
-(* Errnum from zmq side. *)
-// ZMQ_EXPORT int zmq_errno (void);
 function zmq_errno: Integer; cdecl; external LIBZMQ;
-
-(*  Resolves system errors and 0MQ errors to human-readable string. *)
-//ZMQ_EXPORT const char *zmq_strerror (int errnum);
 function zmq_strerror(errnum: Integer): PChar; cdecl; external LIBZMQ;
 
 {-----------------------------------}
 {-----------------------------------}
 { Message functions                 }
 {-----------------------------------}
-//ZMQ_EXPORT int zmq_msg_init (zmq_msg_t *msg);
 function zmq_msg_init(msg: PZMQMsg): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_msg_init_size (zmq_msg_t *msg, size_t size);
 function zmq_msg_init_size(msg: PZMQMsg; size: Cardinal): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_msg_init_data (zmq_msg_t *msg, void *data,
-//    size_t size, zmq_free_fn *ffn, void *hint);
 function zmq_msg_init_data(msg: PZMQMsg; data: Pointer; size: Cardinal;
   zmq_free_fn: TZMQFreeFn; hint: Pointer): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_msg_close (zmq_msg_t *msg);
 function zmq_msg_close(msg: PZMQMsg): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_msg_move (zmq_msg_t *dest, zmq_msg_t *src);
 function zmq_msg_move(dest, src: PZMQMsg): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_msg_copy (zmq_msg_t *dest, zmq_msg_t *src);
 function zmq_msg_copy(dest, src: PZMQMsg): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT void *zmq_msg_data (zmq_msg_t *msg);
 function zmq_msg_data(msg: PZMQMsg): Pointer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT size_t zmq_msg_size (zmq_msg_t *msg);
 function zmq_msg_size(msg: PZMQMsg): Cardinal; cdecl; external LIBZMQ;
 
 {-----------------------------------}
@@ -201,49 +190,28 @@ function zmq_msg_size(msg: PZMQMsg): Cardinal; cdecl; external LIBZMQ;
 { 0MQ infrastructure initialisation }
 { & termination.                    }
 {-----------------------------------}
-//ZMQ_EXPORT void *zmq_init (int io_threads);
 function zmq_init(io_threads: Integer): Pointer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_term (void *context);
 function zmq_term(context: Pointer): Integer; cdecl; external LIBZMQ;
 
 {-----------------------------------}
 {-----------------------------------}
 { 0MQ socket.                       }
 {-----------------------------------}
-//ZMQ_EXPORT void *zmq_socket (void *context, int type);
 function zmq_socket(context: Pointer; itype: Integer): Pointer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_close (void *s);
 function zmq_close(s: Pointer): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_setsockopt (void *s, int option, const void *optval,
-//    size_t optvallen);
 function zmq_setsockopt(s: Pointer; option: Integer; const optval: Pointer;
   optvallen: Cardinal): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_getsockopt (void *s, int option, void *optval,
-//    size_t *optvallen);
 function zmq_getsockopt(s: Pointer; option: Integer; optval: Pointer;
   var optvallen: Cardinal): Integer; cdecl; external LIBZMQ;
-   
-//ZMQ_EXPORT int zmq_bind (void *s, const char *addr);
 function zmq_bind(s: Pointer; const addr: PChar): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_connect (void *s, const char *addr);
 function zmq_connect(s: Pointer; const addr: PChar): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_send (void *s, zmq_msg_t *msg, int flags);
 function zmq_send(s: Pointer; msg: PZMQMsg; flags: Integer): Integer; cdecl; external LIBZMQ;
-
-//ZMQ_EXPORT int zmq_recv (void *s, zmq_msg_t *msg, int flags);
 function zmq_recv(s: Pointer; msg: PZMQMsg; flags: Integer): Integer; cdecl; external LIBZMQ;
 
 {-----------------------------------}
 {-----------------------------------}
 { I/O multiplexing.                 }
 {-----------------------------------}
-//ZMQ_EXPORT int zmq_poll (zmq_pollitem_t *items, int nitems, long timeout);
 function zmq_poll(items: PZMQPollItem; nitems: Integer;
   timeout: Longint): Integer; cdecl; external LIBZMQ;
 
@@ -251,10 +219,8 @@ function zmq_poll(items: PZMQPollItem; nitems: Integer;
 {-----------------------------------}
 { Built-in devices                  }
 {-----------------------------------}
-//ZMQ_EXPORT int zmq_device (int device, void * insocket, void* outsocket);
 function zmq_device(device: Integer; insocket,
   outsocket: Pointer): Integer; cdecl; external LIBZMQ;
-
 
 implementation
 
